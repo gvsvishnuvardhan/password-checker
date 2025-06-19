da84cd556e978f4dc2ef5285d1028b14cca855bc
vijayraghuveer <rveer7052@gmail.com>
2025-06-19 19:55:49 +0530

    updated app  again1

--- app/main.py
+++ app/main.py
@@ -62,4 +62,5 @@ def check_password():
     return render_template_string(HTML, result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    port = int(os.environ.get("PORT", 8080))  
+    app.run(host="0.0.0.0", port=port)
